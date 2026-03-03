d37c08211ef9f56d1725297281e2e1eef0c966c7
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-03-03 11:35:32 +0200

    ft: photos

--- src/test/java/com/hasby/apitest/photos/PhotoDataProvider.java
+++ src/test/java/com/hasby/apitest/photos/PhotoDataProvider.java
@@ -1,4 +1,23 @@
 package com.hasby.apitest.photos;
 
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
 public class PhotoDataProvider {
+    static Stream<Arguments> validPhotoIds() {
+        return Stream.of(
+                Arguments.of(1, 1),
+                Arguments.of(2, 1),
+                Arguments.of(51, 2)
+        );
+    }
+
+    static Stream<Arguments> invalidPhotoIds() {
+        return Stream.of(
+                Arguments.of(99999),
+                Arguments.of(0),
+                Arguments.of(-1)
+        );
+    }
 }
--- src/test/java/com/hasby/apitest/photos/PhotoPayload.java
+++ src/test/java/com/hasby/apitest/photos/PhotoPayload.java
@@ -1,4 +1,25 @@
 package com.hasby.apitest.photos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhotoPayload {
+    public static Map<String, Object> create(){
+        Map<String, Object> photo = new HashMap<>();
+        photo.put("albumId", 1);
+        photo.put("title", "Test Photo Title");
+        photo.put("url", "https://via.placeholder.com/600/test");
+        photo.put("thumbnailUrl", "https://via.placeholder.com/150/test");
+        return photo;
+    }
+
+    public static Map<String, Object> update() {
+        Map<String, Object> photo = new HashMap<>();
+        photo.put("albumId", 1);
+        photo.put("id", 1);
+        photo.put("title", "Updated Photo Title");
+        photo.put("url", "https://via.placeholder.com/600/updated");
+        photo.put("thumbnailUrl", "https://via.placeholder.com/150/updated");
+        return photo;
+    }
 }
