ab390bb77a3abc76714f4eaf7b0564ffe17224ae
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-03-03 12:35:12 +0200

    ft: users

--- src/test/java/com/hasby/apitest/users/UserDataProvider.java
+++ src/test/java/com/hasby/apitest/users/UserDataProvider.java
@@ -1,4 +1,23 @@
 package com.hasby.apitest.users;
 
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
 public class UserDataProvider {
+    static Stream<Arguments> validUserIds() {
+        return Stream.of(
+                Arguments.of(1, "Leanne Graham", "Bret"),
+                Arguments.of(2, "Ervin Howell", "Antonette"),
+                Arguments.of(3, "Clementine Bauch", "Samantha")
+        );
+    }
+
+    static Stream<Arguments> invalidUserIds() {
+        return Stream.of(
+                Arguments.of(999),
+                Arguments.of(0),
+                Arguments.of(-1)
+        );
+    }
 }
--- src/test/java/com/hasby/apitest/users/UserPayload.java
+++ src/test/java/com/hasby/apitest/users/UserPayload.java
@@ -1,4 +1,23 @@
 package com.hasby.apitest.users;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserPayload {
+    public static Map<String, Object> create() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "Test User");
+        user.put("username", "testuser");
+        user.put("email", "test@example.com");
+        return user;
+    }
+
+    public static Map<String, Object> update() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("id", 1);
+        user.put("name", "Updated User");
+        user.put("username", "updateduser");
+        user.put("email", "updated@example.com");
+        return user;
+    }
 }
